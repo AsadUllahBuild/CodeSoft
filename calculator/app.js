2d1139d48e2a0d470735c591f61393e570b499a8
AsadUllahBuild <asadbuild97@gmail.com>
2023-12-23 16:55:13 +0500

    calculator

--- calculator/app.js
+++ calculator/app.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
       button.addEventListener('click', () => {
         const value = button.getAttribute('data-value');
   
-        // Sirf numbers, decimal point, aur kuch specific operators ko allow karna
         const validInputs = /[0-9./*+-]/;
         if (validInputs.test(value)) {
           display.value += value;
@@ -25,9 +24,31 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   
     display.addEventListener('input', function() {
-      // Sirf numbers, decimal point, aur kuch specific operators ko allow karna
       const regex = /[^0-9./*+-]/g;
       this.value = this.value.replace(regex, '');
+    });
+  
+    // The code to handle keyboard input
+
+    document.addEventListener('keydown', function(event) {
+      const key = event.key;
+      const validKeys = /[0-9./*+\-=]|Backspace|Enter|Escape/;
+  
+      if (validKeys.test(key)) {
+        if (key === 'Enter') {
+          try {
+            display.value = eval(display.value);
+          } catch (error) {
+            display.value = 'Error';
+          }
+        } else if (key === 'Escape') {
+          display.value = '';
+        } else if (key === 'Backspace') {
+          display.value = display.value.slice(0, -1);
+        } else {
+          display.value += key;
+        }
+      }
     });
   });
   
